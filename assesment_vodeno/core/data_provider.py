3dd69092a86c87573168982cb62991146bcfe1ee
kesim <kesimenator@gmail.com>
2022-04-26 22:39:29 +0200

    simplified with parametrized

--- assesment_vodeno/core/data_provider.py
+++ assesment_vodeno/core/data_provider.py
@@ -2,27 +2,28 @@ import json
 
 import requests
 
+url = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
+
 
 def checkstatusok(response, param):
     status = response.status_code
     assert (status == 200), param
-    pass  #  todo?
+    pass
 
 
 def get_pokemon(name=None):
     if name is None:
         print('Please deliver the pokemon name!!!')
         return
-    url = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
     response = requests.get(url)
     checkstatusok(response, 'Can NOT get response!')
     an_array = json.loads(response.text)['results']
     for ar in an_array:
         if ar.get('name') == name:
+            print(ar.get('name'))
             return ar
-        else:
-            print('Can not find the pokemon with a given name!!!')
-            return ''
+    print('Can not find the pokemon with a given name!!!')
+    return ''
 
 
 def get_pokemon_details(pokemon=None):
@@ -31,27 +32,19 @@ def get_pokemon_details(pokemon=None):
         return
 
     pok = get_pokemon(pokemon)
-
-    name = pok['name']
-    print(f'pokemon name {name}')
-    url = pok['url']
-    response = requests.get(url)
+    _url = pok.__getitem__('url')
+    response = requests.get(_url)
     checkstatusok(response, 'Can NOT get response!')
     return json.loads(response.text)
 
 
-
-
-# class DataProvider:
-#     def __init__(self):
-#         self.aaa = 1
-#         print("aaa")
-
-
 if __name__ == '__main__':
-    # pokemon = get_pokemon("electrode")
-    # print(pokemon)
     pokemon_details = get_pokemon_details("electrode")
     print(pokemon_details['abilities'])
     print(pokemon_details['height'])
+    response = requests.get(url)
+    an_array = json.loads(response.text)['results']
+
+    for ar in an_array:
+        print(ar)
     # print(pokemon_details['haveAbilityImposter'])
--- assesment_vodeno/tests/test_assesment.py
+++ assesment_vodeno/tests/test_assesment.py
@@ -1,25 +1,30 @@
-import unittest
-
 import pytest
 
+from core.data_provider import get_pokemon_details
 
-@pytest.mark.usefixtures("pokemon_check_fixt")
-class Assessment(unittest.TestCase):
-
-    @pytest.mark.functional
-    def test_01_list_of_abilities(self):
-        _abilities = self.pokemon_details['abilities']
-        print(_abilities)
+testdata = [
+    "electrode",
+    "exeggcute",
+]
 
 
-    @pytest.mark.functional
-    def test_02_height(self):
-        _height = self.pokemon_details['height']
-        print(_height)
+@pytest.mark.functional
+@pytest.mark.parametrize("pokemon_name", testdata)
+def test_01_list_of_abilities(pokemon_name):
+    _abilities = get_pokemon_details(pokemon_name)['abilities']
+    print(_abilities)
 
 
-    @pytest.mark.functional
-    def test_03_ability_imposter(self):
-        pass
+@pytest.mark.functional
+@pytest.mark.parametrize("pokemon_name", testdata)
+def test_02_height(pokemon_name):
+    _height = get_pokemon_details(pokemon_name)['height']
+    print(_height)
+
+
+@pytest.mark.functional
+@pytest.mark.parametrize("pokemon_name", testdata)
+def test_03_ability_imposter(pokemon_name):
+    pass
 
 
