3bdcea0ad88adb5992572c96e85039a16a173fd7
kesim <kesimenator@gmail.com>
2022-04-26 22:59:27 +0200

    logger added

--- assesment_vodeno/core/data_provider.py
+++ assesment_vodeno/core/data_provider.py
@@ -1,8 +1,11 @@
 import json
-
+import logging
 import requests
 
 url = 'https://pokeapi.co/api/v2/pokemon?offset=100&limit=100'
+
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
 def checkstatusok(response, param):
@@ -13,24 +16,23 @@ def checkstatusok(response, param):
 
 def get_pokemon(name=None):
     if name is None:
-        print('Please deliver the pokemon name!!!')
+        logging.warning('Please deliver the pokemon name!!!')
         return
     response = requests.get(url)
     checkstatusok(response, 'Can NOT get response!')
     an_array = json.loads(response.text)['results']
     for ar in an_array:
         if ar.get('name') == name:
-            print(ar.get('name'))
+            logging.info(ar.get('name'))
             return ar
-    print('Can not find the pokemon with a given name!!!')
+    logging.info('Can not find the pokemon with a given name!!!')
     return ''
 
 
 def get_pokemon_details(pokemon=None):
     if pokemon is None:
-        print('Did not received pokemon !!!')
+        logging.warning('Did not received pokemon !!!')
         return
-
     pok = get_pokemon(pokemon)
     _url = pok.__getitem__('url')
     response = requests.get(_url)
@@ -40,11 +42,11 @@ def get_pokemon_details(pokemon=None):
 
 if __name__ == '__main__':
     pokemon_details = get_pokemon_details("electrode")
-    print(pokemon_details['abilities'])
-    print(pokemon_details['height'])
+    logging.info(pokemon_details['abilities'])
+    logging.info(pokemon_details['height'])
     response = requests.get(url)
     an_array = json.loads(response.text)['results']
 
     for ar in an_array:
-        print(ar)
+        logging.info(ar)
     # print(pokemon_details['haveAbilityImposter'])
